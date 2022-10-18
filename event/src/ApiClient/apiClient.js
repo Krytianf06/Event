cb92fc4c95df4af1a2e0322d1445bb9736c34874
Krystian Florkowski <86536950+Krytianf06@users.noreply.github.com>
2022-10-18 12:55:31 +0200

    coś

--- event/src/ApiClient/apiClient.js
+++ event/src/ApiClient/apiClient.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import axios from 'axios';
+
+
 
 function Dane(){
-    try {
-        
-    } catch (error) {
-        
-    }
-    return ();
-}
+    return fetch('https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=r0xZ38lkAmbiv0rfdbq9gE2ORGZIsAvL')
+    .then(response => {
+        const data = response.json();
+        console.log(data)
+         return data; })
+    .catch(error => console.log(error)
+    )};
+
 
 export default Dane;
