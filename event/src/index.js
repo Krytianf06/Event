b39d6115ddc5cbb57469c880a223cbddf71012ce
Krystian Florkowski <86536950+Krytianf06@users.noreply.github.com>
2022-10-20 01:22:28 +0200

    start

--- event/src/index.js
+++ event/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 // import { Provider } from 'react-redux';
-import App from './App';
+import Header from './Header';
 import './index.css';
 
 const container = document.getElementById('root');
@@ -9,6 +9,6 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-      <App />
+      <Header />
   </React.StrictMode>
 );
